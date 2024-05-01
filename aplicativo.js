9cc9b07e0fe7f99b9ce124ef11cd947b98e44b2f
andre23a75 <164100554+andre23a75@users.noreply.github.com>
2024-05-01 11:58:32 -0300

    Update aplicativo.js
    mudança de texto

--- aplicativo.js
+++ aplicativo.js
@@ -4,7 +4,7 @@ var http = require('http');
 // A função de callback será executada sempre que alguém tentar acessar o computador na porta 8080.
 http.createServer(function (req, res) {
   // Escreva uma resposta para o cliente
-  res.write('Olá, bem-vindo à minha aplicação web!'); 
+  res.write('Olá, bem-vindo à minha aplicação web teste!'); 
   // Fim da resposta
   res.end(); 
 }).listen(8080); // O servidor irá ouvir na porta 8080
